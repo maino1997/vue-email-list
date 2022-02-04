dc3786dd66d840b749b1e3084ab7097a8367fea2
Sasha Mainardi <sashamainardi4@gmail.com>
2022-02-04 17:55:43 +0100

    maxMail variable added

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
     data: {
-        mailList: []
+        mailList: [],
+        maxMail: 10,
     },
     methods: {
         getRandomMail() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
                 const mail = res.data.response;
+                // while (this.mailList.length + 1 < this.maxMail) {
+
+                console.log(this.mailList.length);
+
                 if (!this.mailList.includes(mail)) {
                     this.mailList.push(mail);
                 }
+                // }
+
             })
         }
     },
     created() {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < this.maxMail; i++) {
             this.getRandomMail();
         }
     }
